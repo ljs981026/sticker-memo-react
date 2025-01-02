54d3c554aac66d6da3c2a982a633a8b627cb9a8b
ljs981026 <ljs981026@naver.com>
2025-01-03 00:00:39 +0900

    최초 커밋 드래그 컴포넌트 생성

---
+++ src/lib/Draggable.jsx
@@ -0,0 +1,50 @@
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import "./Draggable.css"
+import { debounce } from 'underscore';
+const Draggable = ({children, handleRef, onMove, x, y}) => {
+  const dragRef = useRef(null);
+  const initialX = useRef(0);
+  const initialY = useRef(0);
+  const [position, setposition] = useState({x, y});
+
+  const Move = useMemo(() => debounce((x,y) => onMove(x,y), 500), [onMove])
+
+  const onMouseMove = useCallback((e) => {
+    setposition({
+      x: e.clientX - initialX.current,
+      y: e.clientY - initialY.current
+    })    
+    Move(e.clientX - initialX.current, e.clientY - initialY.current);
+  },[Move])
+
+  const removeEvents = useCallback(() => {
+    document.removeEventListener("mousemove", onMouseMove);
+    document.removeEventListener("mouseup", removeEvents);
+    document.body.removeEventListener("mouseleave", removeEvents);    
+  }, [onMouseMove])
+
+  const onMouseDown = useCallback((e) => {
+    const {left, top} = dragRef.current.getBoundingClientRect();
+    initialX.current = e.clientX - left;
+    initialY.current = e.clientY - top;
+    document.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseup", removeEvents);
+    document.body.addEventListener("mouseleave", removeEvents);
+  }, [onMouseMove, removeEvents])
+
+  useEffect(() => {
+    const handle = handleRef.current;
+    handle.addEventListener("mousedown", onMouseDown);
+    return () => {
+      handle.removeEventListener("mousedown", onMouseDown);
+    }
+  }, [handleRef, onMouseDown])
+
+  return (
+    <div ref={dragRef} className='draggable' style={{transform: `translate(${position.x}px, ${[position.y]}px)`}}>
+      {children}
+    </div>
+  )
+}
+
+export default Draggable
